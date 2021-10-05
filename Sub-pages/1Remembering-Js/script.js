5d0480f6a14b7d757d30d15cf2dac01331926d31
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2021-10-05 18:49:13 -0300

    math calculator

--- Sub-pages/1Remembering-Js/script.js
+++ Sub-pages/1Remembering-Js/script.js
@@ -38,3 +38,31 @@ function get() {
 const lgButton = document.querySelector('#lg-button')
 const lgOutput = document.querySelector('#lg-output')
 lgButton.addEventListener('click', get)
+// Calculator section 
+function calc() {
+    let n1 = parseInt(document.getElementById('n1').value) 
+    let n2 = parseInt(document.getElementById('n1').value)
+    let mthd = document.getElementById('type-c').value
+    if (n1.length === 0 | n2.length === 0 | mthd.length === 0) {
+        window.alert('Error, fill the calculator inputs.')
+    } else if (mthd != '+' && mthd != '-' && mthd != '*' && mthd != '/') {
+        window.alert('Error, Mthd is incorrect, use one of the options.')
+    } else {
+        if (mthd === '+') {
+            document.getElementById('calc-r').innerHTML = n1 + n2
+        } else if (mthd === '-') {
+            document.getElementById('calc-r').innerHTML = n1 - n2
+        } else if (mthd === '*') {
+            document.getElementById('calc-r').innerHTML = n1 * n2
+        } else if (mthd === '/') {
+            document.getElementById('calc-r').innerHTML = n1 / n2
+        }
+    }
+    document.getElementById('n1').value = ''
+    document.getElementById('n2').value = ''
+    document.getElementById('type-c').value = ''
+}
+
+
+const ClButton = document.querySelector('#rbutton')
+ClButton.addEventListener('click', calc)
