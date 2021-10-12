098723c18c1c7fc46bfbcabd1256c3dfd4be396c
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2021-10-11 23:02:25 -0300

    add atribute working

--- Sub-pages/2Working-with-objects/script.js
+++ Sub-pages/2Working-with-objects/script.js
@@ -12,8 +12,7 @@ function step1() {
     adAtb.style.backgroundColor = '#00ad009d'
     adAtb.style.color = 'white'
     adAtb.style.textShadow = '1px 1px 2px black'
-    preview = document.getElementById('display-p')
-    let object = new Object()
+    object = new Object()
     preview.innerHTML = printObject(object)
 }
 function printObject(o) {
@@ -21,9 +20,17 @@ function printObject(o) {
     for (let p in o) {data += `\'${p}\'` + ': ' + o[p] + ', '}
     return(`{ ${data.slice(0, -2)} }`) 
 }
-    
+function addAtribute() {
+    let key = document.getElementById('obj-key').value
+    let value = document.getElementById('obj-key-val').value
+    object[key] = value
+    preview.innerHTML = printObject(object)
+}
+let object = ''
+const preview = document.getElementById('display-p')
 const newObject = document.getElementById('new-ob-btn')
 const adAtb = document.getElementById('add-atb-btn')
 const clObj = document.getElementById('cl-obj')
 const nextStep1 = document.getElementById('obj-save-btn')
 newObject.addEventListener('click', step1)
+adAtb.addEventListener('click', addAtribute)
