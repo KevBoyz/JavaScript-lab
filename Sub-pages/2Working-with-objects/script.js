eadf05713b428f8e3bac3dfed4bead8ee4bca8b0
KevBoyz <71911085+KevBoyz@users.noreply.github.com>
2021-10-11 22:52:07 -0300

    object factory first part

---
+++ Sub-pages/2Working-with-objects/script.js
@@ -0,0 +1,29 @@
+// Object Factory section
+function step1() {
+    newObject.style.backgroundColor = '#636363'
+    newObject.style.color = '#454545'
+    newObject.style.textShadow = 'none'
+    newObject.disabled = true
+    document.getElementById('obj-key').disabled = false
+    document.getElementById('obj-key-val').disabled = false
+    clObj.disabled = false
+    nextStep1.disabled = false
+    adAtb.disabled = false
+    adAtb.style.backgroundColor = '#00ad009d'
+    adAtb.style.color = 'white'
+    adAtb.style.textShadow = '1px 1px 2px black'
+    preview = document.getElementById('display-p')
+    let object = new Object()
+    preview.innerHTML = printObject(object)
+}
+function printObject(o) {
+    let data = ''
+    for (let p in o) {data += `\'${p}\'` + ': ' + o[p] + ', '}
+    return(`{ ${data.slice(0, -2)} }`) 
+}
+    
+const newObject = document.getElementById('new-ob-btn')
+const adAtb = document.getElementById('add-atb-btn')
+const clObj = document.getElementById('cl-obj')
+const nextStep1 = document.getElementById('obj-save-btn')
+newObject.addEventListener('click', step1)
